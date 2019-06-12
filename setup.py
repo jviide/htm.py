3a36affb529dc37629002ef125466f68c0b656fd
Joachim Viide <jviide@iki.fi>
2019-06-13 01:58:18 +0300

    v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="htm",
-    version="0.0.3",
+    version="0.0.4",
     author="Joachim Viide",
     author_email="jviide@iki.fi",
     description="HTM for Python",
